0d1d5578b1d80142fb7347128d3377406d3b4a68
bobbyg <garza4@southwestern.edu>
2019-02-19 10:01:29 -0600

    Started working in bobbyBranch. Added comments...

---
+++ app/src/main/java/com/example/newme/Login.java
@@ -0,0 +1,13 @@
+package com.example.newme;
+
+public class Login {
+
+    //push information onto the blockchain.
+    //need to be able to verify user
+    //{"Bobby Garza", Pin, uniqueID}
+    //uniqueID will be used to verify transactions.
+    //create a user class...
+    public static String test(){
+        return "Hello";
+    }
+}
--- app/src/main/java/com/example/newme/MainActivity.java
+++ app/src/main/java/com/example/newme/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        System.out.println(Login.test());
+        System.out.println("something");
     }
 }
