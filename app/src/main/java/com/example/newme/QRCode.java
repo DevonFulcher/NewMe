be0e0bd047fabdfacc3594a37133cce6efa8ee75
maldonad <44204770+maldonad@users.noreply.github.com>
2019-02-21 15:40:30 -0600

    Had to add file to be updated in git

---
+++ app/src/main/java/com/example/newme/QRCode.java
@@ -0,0 +1,13 @@
+package com.example.newme;
+
+public class QRCode extends MainActivity{
+
+    public static String var = "Seriously";
+
+
+    QRCode(){
+
+    }
+
+    static String getVar() {return var; }
+}
