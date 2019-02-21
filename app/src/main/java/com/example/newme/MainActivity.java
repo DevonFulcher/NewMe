0120d1a466ee998b7e75d8a0fa579179a7ad4233
maldonad <44204770+maldonad@users.noreply.github.com>
2019-02-21 15:22:24 -0600

    .

--- app/src/main/java/com/example/newme/MainActivity.java
+++ app/src/main/java/com/example/newme/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.newme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView greeting;
+    Button click;
+    QRCode test = new QRCode();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        greeting = (TextView)findViewById(R.id.tvGreeting);
+        click = (Button)findViewById(R.id.btnClick);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                greeting.setVisibility(View.VISIBLE);
+                greeting.append(test.getVar());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/newme/ProfilePage.java
+++ app/src/main/java/com/example/newme/ProfilePage.java
@@ -1,2 +1,4 @@
+package com.example.newme;
+
 public class ProfilePage {
 }
