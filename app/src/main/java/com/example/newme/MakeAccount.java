3e3f95d22123a5c2bee2980018d904474d94342d
bobbyg <garza4@southwestern.edu>
2019-03-03 16:58:10 -0600

    fake DB working, and so is switching, need to plug in Adanna's code for login?

--- app/src/main/java/com/example/newme/MakeAccount.java
+++ app/src/main/java/com/example/newme/MakeAccount.java
@@ -4,17 +4,27 @@ import android.content.Context;
 import android.os.Bundle;
 //import android.support.design.widget.FloatingActionButton;
 //import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.HashSet;
 import java.util.Set;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 
 public class MakeAccount extends AppCompatActivity {
 
+    private static Set bigchainDB = new HashSet<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,46 +32,101 @@ public class MakeAccount extends AppCompatActivity {
         Intent intent = getIntent();
         intent.getAction();
 
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
+        final Button button = findViewById(R.id.create_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence acct_exists = "This account already exists";
+                int duration =Toast.LENGTH_LONG;
+                Toast acctToast = Toast.makeText(context,acct_exists,duration);
+
+                if(hashData().equals(null)){
+                    acctToast.show();//this account already exists
+
+                }else{
+                    Log.d("check","add here \n");
+                    MakeAccount.this.bigchainDB.add(hashData());
+                    MakeAccount.this.finish();
+                }
+
+            }
+        });
+
+
+    }
+
+    private boolean makeAcct(){
+        return true;
     }
 
 
-    private void hashData(){
-        int first = R.id.first_name;
-        int last = R.id.last_name;
-        int email = R.id.email;
-        int pin = R.id.pin;
-        int hash = first + last + email + pin;
+    private String hashData(){
 
 
+        TextInputLayout first = (TextInputLayout) findViewById(R.id.first_name);
+        TextInputLayout last = (TextInputLayout)findViewById(R.id.last_name);
+        EditText e = (EditText)findViewById(R.id.email);  //email
+        EditText p = (EditText) findViewById(R.id.user_pin); //pin
+
+
+        String UFirst = first.getEditText().getText().toString();
+        String ULast = last.getEditText().getText().toString();
+        String UEmail = e.getText().toString();
+        String UPin = p.getText().toString();
+
+        Log.d("FName",UFirst);
         int duration =Toast.LENGTH_LONG;
-
-        Set bigChainDB = new HashSet<Integer>();
-
-
         Context context = getApplicationContext();
         CharSequence text = "Pin Doesn't Match";
         CharSequence acceptedText = "Pin Accepted";
         CharSequence userExists = "This user already has an account";
+        CharSequence added = "Added user!";
+        Toast added_to_DB = Toast.makeText(context,added,duration);
         Toast toast = Toast.makeText(context,text,duration);
         Toast accToast = Toast.makeText(context,acceptedText,duration);
         Toast existToast = Toast.makeText(context,userExists,duration);
 
-        if(R.id.confirm_pin != pin){
+        if(R.id.confirm_pin != R.id.pin){
             toast.show();
-            hashData();
         }else{
             accToast.show();
         }
 
-        if(bigChainDB.contains(hash)){
-            existToast.show();
-        }else{
-            bigChainDB.add(hash);
+        String to_hash = UFirst+ ULast + UEmail + UPin;
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("Sha-256");
+            byte[] user_hash = md.digest(to_hash.getBytes());
+            BigInteger big_num = new BigInteger(1, user_hash);
+            String hashed_val = big_num.toString(16);
+
+            while (hashed_val.length() < 32) {
+                hashed_val = "O" + hashed_val;
+            }
+            if(MakeAccount.this.bigchainDB.contains(hashed_val)){
+                existToast.show();
+                return null;
+            }
+            else{
+                added_to_DB.show();
+            }
+
+
+            Log.d("hash",hashed_val);
+            return hashed_val;
+
+        }catch (NoSuchAlgorithmException al){
+            System.out.println("rip" + al);
+            return null;
         }
+
+
 
 
     }
