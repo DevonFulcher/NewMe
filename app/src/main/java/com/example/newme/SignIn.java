5e0740a25eba96b194d362273d63cde1d9229def
bobbyg <garza4@southwestern.edu>
2019-03-07 17:06:51 -0600

    pushing to practice good pushing habits

--- app/src/main/java/com/example/newme/SignIn.java
+++ app/src/main/java/com/example/newme/SignIn.java
@@ -1,5 +1,10 @@
 package com.example.newme;
 
+import android.os.Bundle;
+
 public class SignIn {
     //if user enters the correct pin then display the profile page... If user is not in the database, make sure to not allow sign in
+    protected void onCreate(Bundle savedInstanceState) {
+        
+    }
 }
