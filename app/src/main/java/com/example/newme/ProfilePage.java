cdad720fb92493279017bdc0d848db078d965b6d
Adanna <courta@gmail.com>
2019-04-04 14:47:47 -0500

    getting information from make account for profile page

--- app/src/main/java/com/example/newme/ProfilePage.java
+++ app/src/main/java/com/example/newme/ProfilePage.java
@@ -3,11 +3,17 @@ package com.example.newme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.zxing.Result;
 
 public class ProfilePage  extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_page);
+        MainActivity.Name.setText(MakeAccount.user.firstName + " " + MakeAccount.user.lastName);
+        MainActivity.Email.setText(MakeAccount.user.email);
+
     }
 }
